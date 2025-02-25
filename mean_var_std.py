a274415d5f99747a86de0548d73842a71ea64607
FelipeTlusty <tlustyfelipe@gmail.com>
2025-02-24 18:38:07 -0600

    submission

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,7 +2,29 @@ import numpy as np
 
 def calculate(list):
 
+    if len(list) != 9:
+        raise ValueError("List must contain nine numbers.")
+    
+    matrix = np.array(list)
+    re_matrix = matrix.reshape(3,3)
+    calculations = {}
+    
+    # Mean
+    calculations['mean'] = [np.mean(re_matrix, axis = 0).tolist(), np.mean(re_matrix, axis = 1).tolist(), np.mean(matrix)]
 
+    # Variance
+    calculations['variance'] = [np.var(re_matrix, axis = 0).tolist(), np.var(re_matrix, axis = 1).tolist(), np.var(matrix)]
 
+    # Std Deviation
+    calculations['standard deviation'] = [np.std(re_matrix, axis = 0).tolist(), np.std(re_matrix, axis = 1).tolist(), np.std(matrix)]
+
+    # Max
+    calculations['max'] = [np.max(re_matrix, axis = 0).tolist(), np.max(re_matrix, axis = 1).tolist(), np.max(matrix)]
+
+    # Min
+    calculations['min'] = [np.min(re_matrix, axis = 0).tolist(), np.min(re_matrix, axis = 1).tolist(), np.min(matrix)]
+
+    # Sum
+    calculations['sum'] = [np.sum(re_matrix, axis = 0).tolist(), np.sum(re_matrix, axis = 1).tolist(), np.sum(matrix)]
 
     return calculations
